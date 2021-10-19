e7eb198e7214ab7b5a88050a905a0611362329bc
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2021-10-19 11:33:26 +0200

    Do exercise Zadanie_3

---
+++ Zadanie_3/Zadanie_3/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Zadanie_3
+//
+//  Created by Modzel on 18/10/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Zadanie_3/Zadanie_3/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Zadanie_3
+//
+//  Created by Modzel on 18/10/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UIButton *informationButton;
+@property (nonatomic, weak) IBOutlet UIImageView *image;
+
+@end
+
